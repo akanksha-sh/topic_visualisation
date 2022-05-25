bd9dd9f9961772bb8e8f7315a817e54bc76cfd80
akanksha-sh <52570629+akanksha-sh@users.noreply.github.com>
2022-05-25 20:41:25 +0100

    add local repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import D3Component from "./D3Component";
+import './App.css';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <D3Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Standard version
+import 'socicon/css/socicon.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './assets/plugins/line-awesome/css/line-awesome.css';
+import './assets/plugins/flaticon/flaticon.css';
+import './assets/plugins/flaticon2/flaticon.css';
+import './assets/plugins/flaticon2/flaticon2.css';
+import './assets/plugins/flaticon2/flaticon3.css';
+import './assets/plugins/font-awesome/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <App basename={'/'} />,
+  document.getElementById('root')
+);
